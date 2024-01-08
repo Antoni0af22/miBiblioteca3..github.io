a6b17dd9676ba2885f029afacb9c745578227ae3
Antoni0af22 <62048162+Antoni0af22@users.noreply.github.com>
2024-01-07 23:24:05 -0600

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -3,7 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
 
 const firebaseConfig = {
-  // Paste your firebase config here
+  apiKey: "AIzaSyCDBgrwLltblT6_xY5gdrjCQRgwWno8GXM",
+  authDomain: "mibilioteca3.firebaseapp.com",
+  projectId: "mibilioteca3",
+  storageBucket: "mibilioteca3.appspot.com",
+  messagingSenderId: "791987332781",
+  appId: "1:791987332781:web:e1345155eae509f5e0c824"
 };
 
 // Initialize Firebase
